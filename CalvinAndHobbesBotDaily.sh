63bc96d89ed5fbd8d8d66e8959f818c6c059bf84
Marvin <marv42+github@gmail.com>
2020-11-28 21:14:41 +0100

    use dirname of scripts
    back to bash scripts

--- CalvinAndHobbesBotDaily.sh
+++ CalvinAndHobbesBotDaily.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-python3 CalvinAndHobbesBot.py
+# TODO https://stackoverflow.com/a/246128
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+python3 $DIR/CalvinAndHobbesBot.py
