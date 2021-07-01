62a4bf1aec460c760f138b6df4440282e43e3aee
Marvin <marv42+github@gmail.com>
2021-07-01 10:55:16 +0200

    use venv

--- CalvinAndHobbesBotRandomly.sh
+++ CalvinAndHobbesBotRandomly.sh
@@ -5,5 +5,7 @@
 # TODO https://stackoverflow.com/a/246128
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-RANDOM_NUMBER_FROM_0_TO_11=$(echo $((RANDOM % 12)))
-[ "$RANDOM_NUMBER_FROM_0_TO_11" -eq 0 ] && python3 $DIR/CalvinAndHobbesBot.py --random
+$DIR/venv/bin/activate
+
+RANDOM_NUMBER_FROM_0_TO_35=$(echo $((RANDOM % 36)))
+[ "$RANDOM_NUMBER_FROM_0_TO_35" -eq 0 ] && $DIR/venv/bin/python3 $DIR/CalvinAndHobbesBot.py --random
