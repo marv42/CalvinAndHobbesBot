0342f17d03a7264a4fe2d8fab742a931e60f0e9f
Marvin <marv42+github@gmail.com>
2023-06-05 11:53:27 +0200

    Change working dir

--- CalvinAndHobbesBotRandomly.sh
+++ CalvinAndHobbesBotRandomly.sh
@@ -5,7 +5,8 @@
 # TODO https://stackoverflow.com/a/246128
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-$DIR/venv/bin/activate
+cd $DIR
+source $DIR/venv/bin/activate
 
 RANDOM_NUMBER_FROM_0_TO_35=$(echo $((RANDOM % 36)))
 [ "$RANDOM_NUMBER_FROM_0_TO_35" -eq 0 ] && $DIR/venv/bin/python3 $DIR/CalvinAndHobbesBot.py --random
